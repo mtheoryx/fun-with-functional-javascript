fa5c7567a8c64edb4d91bafab5e8473746e05fd2
David Poindexter <drpoindexter@gmail.com>
2017-04-27 19:48:19 +0000

    Initial Right/left setup and examples

--- lesson-3.js
+++ lesson-3.js
@@ -3,17 +3,29 @@
 const Right = x => 
 ({
     map: f => Right(f(x)),
+    fold: (f, g) => g(x),
     inspect: () => `Right(${x})`
 });
 
 const Left = x => 
 ({
     map: f => Left(x),
+    fold: (f, g) => f(x),
     inspect: () => `Left(${x})`
 });
 
-const resultRight = Right(3).map(x => x + 1);
-console.log(resultRight); //-> Right(4)
+const resultRight = 
+    Right(2)
+    .map(x => x + 1)
+    .map(x => x / 2)
+    .fold(x => 'error', x => x);
+    
+console.log(resultRight); //-> 1.5
 
-const resultLeft = Left(3).map(x => x + 1);
-console.log(resultLeft); //-> Left(3)
+const resultLeft = 
+    Left(2)
+    .map(x => x + 1)
+    .map(x => x / 2)
+    .fold(x => 'error', x => x);
+
+console.log(resultLeft); //-> error
